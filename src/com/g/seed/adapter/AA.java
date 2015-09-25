04a694bf9d45c48872dcb3191dd8fb8fd695c56b
zhigeng.ni <zhigeng.ni@b12f1491-0459-4b77-aefd-081ad6a019d5>
2015-09-25 09:28:03 +0000

    commit test
    git-svn-id: svn://172.16.8.211/usr/www/svn/android/common/trunk/seed_1.0@4463 b12f1491-0459-4b77-aefd-081ad6a019d5

---
+++ src/com/g/seed/adapter/AA.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.g.seed.adapter;
+
+/** 
+* @ClassName: AA 
+* @author zhigeng.ni 
+* @date 2015年9月25日 下午5:26:31 
+* @Description: TODO (描述作用) 
+*  
+*/
+public class AA {
+
+}
